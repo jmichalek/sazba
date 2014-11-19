708a79caecee4e476d83f94353aab77b7e3f9672
Jakub Michálek <jakub.michalek@pirati.cz>
2014-11-19 18:02:26 +0100

    Dopis

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-# Instaluje do domovské složky soubor makelatex
+# Instaluje potřební balíky do domovské složky soubor makelatex
+
+sudo apt-get install pandoc texlive-xetex texlive-lang-czechslovak texlive-latex-base
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
